1d29340759a166792d0ea71c92122aa52518048a
artsyvictoriasantana-hub <artsyvictoriasantana@gmail.com>
2026-02-06 13:59:25 -0700

    Update Chessboard.java

--- Chessboard.java
+++ Chessboard.java
@@ -1,3 +1,22 @@
 public class Chessboard {
-    
+    public static int MAX_ROW = 8;
+    public static int MIN_ROW = 1;
+
+
+    public ChessBoard(){
+
+    }
+
+    public boolean withinChessboard(char column, int row){
+
+        if (column < 'a' || column > 'h'){
+            return false;
+        }
+
+        if (row < MIN_ROW || row > MAX_ROW){
+            return false;
+        }
+
+        return false;
+    }
 }
